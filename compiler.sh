3fe4f0b019bf8e16366d9aee1960487aa78ea997
Agustín Dosantos <101806041+dosaagu@users.noreply.github.com>
2026-09-14 10:42:13 -0300

    Create compiler.sh for project compilation
    Added a bash script to compile the project using Flex and GCC.

---
+++ compiler.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Detener el script inmediatamente si ocurre un error
+set -e
+
+# echo "=== 1. Compilando el proyecto ==="
+# echo "[Bison] Generando parser..."
+# bison -d bison.y
+
+echo "[Flex] Generando lexer..."
+flex lex.l
+
+echo "[GCC] Compilando binarios..."
+# gcc -Wall -Wextra -g -o compilador main.c bison.tab.c lex.yy.c ast.c lista.c semantica.c assembly.c
+gcc -Wall -Wextra -g -o compilador lex.yy.c
+echo "✅ Compilacion exitosa."
